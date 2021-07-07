b9c7ace21a7975c378c393520ca44c33aaa19bbc
saad1111112 <87070423+saad1111112@users.noreply.github.com>
2021-07-07 13:49:01 +0300

    Update packet_flow_key.py

--- packet_flow_key.py
+++ packet_flow_key.py
@@ -17,14 +17,11 @@ def get_packet_flow_key(packet, direction) -> tuple:
 		src_ip = packet["IP"].src
 		src_port = packet[protocol].sport
 		dest_port = packet[protocol].dport
-		# from here
 		src_ether= packet["Ether"].src
-		print(packet)
 	else:
 		dest_ip = packet["IP"].src
 		src_ip = packet["IP"].dst
 		src_port = packet[protocol].dport
 		dest_port = packet[protocol].sport
 		src_ether= packet["Ether"].src
-		print(packet)
 	return dest_ip, src_ip, src_port, dest_port, src_ether
